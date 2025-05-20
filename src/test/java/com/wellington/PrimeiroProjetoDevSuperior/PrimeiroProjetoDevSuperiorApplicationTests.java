50007c587d47e377cdab4416b384170b6ece7767
wellington <wellington.h.s.oliveira@hotmail.com>
2025-05-20 12:06:38 -0300

    Project Created

---
+++ src/test/java/com/wellington/PrimeiroProjetoDevSuperior/PrimeiroProjetoDevSuperiorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wellington.PrimeiroProjetoDevSuperior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroProjetoDevSuperiorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
